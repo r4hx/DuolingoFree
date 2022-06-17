c1334990d3a577cff025d1491abef0d4e7f2b47a
Egorov Egor <buggies_aliases.0f@icloud.com>
2022-06-17 16:51:06 +0300

    fix

--- frontend/views.py
+++ frontend/views.py
@@ -10,7 +10,7 @@ from frontend.forms import TaskForm
 
 
 def index(request):
-    max_unique_links_per_day = os.getenv("MAX_UNIQUE_LINKS_PER_DAY", 50)
+    max_unique_links_per_day = os.getenv("MAX_UNIQUE_LINKS_PER_DAY", 100)
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -39,7 +39,7 @@ def index(request):
         running = Task.objects.filter(state=2).count()
         finished = Task.objects.filter(state=3).count()
         errored = Task.objects.filter(state=4).count()
-        start_after = running + queued
+        start_after = (running + queued) * 2
         context = {
             "form": form,
             "queued": queued,
