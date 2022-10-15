b70e0fc996080ddb93704380dcfa44fcb162e40a
Egorov Egor <buggies_aliases.0f@icloud.com>
2022-10-15 08:06:37 +0300

    fix

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,16 +1,9 @@
-import logging
-
 from django.utils import timezone
 from DuolingoFree.celery import app
 
 from backend.duolingo import Duolingo
 from backend.models import Task
 from backend.telegram import Telegram
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-)
 
 
 @app.task(bind=True, ignore_result=False)
@@ -27,7 +20,6 @@ def create_a_new_invited_user(self, pk):
             Telegram().send_message(message=f"Task #{task.pk} - FINISHED")
             return e
     except Exception as e:
-        print(e)
         task.state = 4
         task.save()
         Telegram().send_message(message=f"Task #{task.pk} - ERROR")
