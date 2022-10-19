78130fdff2025f608fc9a2091e055cfe82e3fdb5
Egorov Egor <buggies_aliases.0f@icloud.com>
2022-10-19 05:29:43 +0300

    fixed admin url

--- DuolingoFree/urls.py
+++ DuolingoFree/urls.py
@@ -17,11 +17,9 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("i18n/", include("django.conf.urls.i18n")),
-]
+urlpatterns = []
 urlpatterns += i18n_patterns(
     path("", include("frontend.urls")),
+    path("admin/", admin.site.urls),
     prefix_default_language=True,
 )
