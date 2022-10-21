1d369701a6a0c1f09b1cf5436c6a20ab1dbe43a9
Egorov Egor <buggies_aliases.0f@icloud.com>
2022-10-22 02:48:23 +0300

    fix

--- backend/management/commands/queue_reset.py
+++ backend/management/commands/queue_reset.py
@@ -6,11 +6,18 @@ class Command(BaseCommand):
     help = "Перезагрузка задач в очереди"
 
     def handle(self, *args, **options):
+        # restart queued tasks
         tasks = Task.objects.filter(state=1)
         for t in tasks:
             t.state = 0
             t.save()
+        # restart running tasks
         tasks = Task.objects.filter(state=2)
+        for t in tasks:
+            t.state = 0
+            t.save()
+        # restart errored tasks
+        tasks = Task.objects.filter(state=4)
         for t in tasks:
             t.state = 0
             t.save()
